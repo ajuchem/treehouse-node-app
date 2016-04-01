26a880fea012acd11fae39bcec37c2e3351dd807
ajuchem <ajuchem@daudt.com>
2016-04-01 11:04:43 -0600

    Add var http to be able to access https json and http.STATUS_CODES

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 // Problem: Simple way to look at user's badge count and JS points
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 var https = require("https");
-var username = "alessandradaudt";
+var http = require("http");
+var username = "chalkers1234";
 
+// Print out message
 function printMessage(username, badgeCount, points) {
   var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in JavaScript.";
   console.log(message);
 }
+
+// Print out error messages
+function printError(error) {
+  console.error(error.message);
+}
+
 
 // Connect to the API URL (http://teamtreehouse.com/username.json)
 var req = https.get("https://teamtreehouse.com/" + username + ".json", function(res) {
@@ -16,13 +24,23 @@ var req = https.get("https://teamtreehouse.com/" + username + ".json", function(
     body += chunk;
   })
   res.on('end', function() {
-    // Parse the data
-    var profile = JSON.parse(body);
-    // Print the data
-    printMessage(username, profile.badges.length, profile.points.JavaScript);
+    if (res.statusCode === 200) {
+      try {
+        var profile = JSON.parse(body);
+        printMessage(username, profile.badges.length, profile.points.JavaScript);
+      } catch(error) {
+        // Parse error
+        printError(error);
+      }
+    } else {
+      // Status code error
+      printError({message: "There was an error getting the profile for " + username + ". (" + http.STATUS_CODES[res.statusCode] +")"});
+    }
+
   })
+  // Parse the data
+  // Print the data
 });
 
-req.on("error", function(error) {
-  console.error(error.message);
-})
+// Connection error
+req.on("error", printError);
